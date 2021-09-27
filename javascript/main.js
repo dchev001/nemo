ebb3aa9739056b05c51c8b67c5486398afe726b1
dchev001 <dchev001@gmail.com>
2021-09-27 15:18:16 -0400

    legend initial

--- javascript/main.js
+++ javascript/main.js
@@ -38,9 +38,63 @@ function showMapSubMenu()
   }
 }
 
+// to show the data layers
+function showDataLayers()
+{
+  var x = document.getElementById("layerConsole");
+  if (x.style.display === "none")
+  {
+    x.style.display = "block";   
+  }
+  else
+  {
+    x.style.display = "none";
+  }
+}
 
 // to change between map styles
 function changeMap(val)
 {
   map.setStyle('mapbox://styles/mapbox/' + val);
 }
+
+// to change the gradient in the layer console
+function changeLegendGradient() 
+{
+  var x = document.getElementById("dataLayer").value;
+  var y = document.getElementById("grad");
+
+  if (x == 'temp')
+  {
+    y.style.background = "linear-gradient(to right, green, white, red)";
+  }
+  else if (x == 'pH_level')
+  {
+    y.style.background = "linear-gradient(to right, blue, white, red)";
+  }
+  else if (x == 'odo')
+  {
+    y.style.background = "linear-gradient(to right, yellow, red, orange)";  
+  }
+  else if (x == 'salinity')
+  {
+    y.style.background = "linear-gradient(to right, green, white, red, green)";
+  }
+  else if (x == 'chlorophyll')
+  {
+    y.style.background = "linear-gradient(to right, blue, white, blue, white)";
+  }
+  else if (x == 'turbidity')
+  {
+    y.style.background = "linear-gradient(to right, blue, red, blue, green)";
+  }
+  else if (x == 'none')
+  {
+    y.style.background = "linear-gradient(to right, white, white)";
+  }
+  else
+  {
+    y.style.background = "linear-gradient(to right, white, white)";
+  }
+}
+
